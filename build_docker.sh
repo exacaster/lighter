2cfd48141d2f6dfb20aec06374396251e52cd5ca
paulius <paulius@exacaster.com>
2021-06-07 11:45:20 +0300

    Add gitlab-ci

--- build_docker.sh
+++ build_docker.sh
@@ -4,10 +4,12 @@ GREEN="32"
 BOLDGREEN="\e[1;${GREEN}m"
 ENDCOLOR="\e[0m"
 
+TAG=${TAG:-latest}
+
 echo -e "${BOLDGREEN}Building server${ENDCOLOR}"
 cd server
 ./gradlew dockerBuild
 cd ..
 
 echo -e "${BOLDGREEN}Building docker with frontend${ENDCOLOR}"
-docker build . -t exacaster/lighter:latest --network host
+docker build . -t exacaster/lighter:${TAG} --network host
