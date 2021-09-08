4a2a4bf5c74a70ffb37e175feb10e8d704fc235b
paulius <paulius@exacaster.com>
2021-09-08 15:00:45 +0300

    add log field for compatibility

--- server/src/main/java/com/exacaster/lighter/application/Application.java
+++ server/src/main/java/com/exacaster/lighter/application/Application.java
@@ -3,6 +3,7 @@ package com.exacaster.lighter.application;
 import com.exacaster.lighter.spark.SubmitParams;
 import com.exacaster.lighter.storage.Entity;
 import java.time.LocalDateTime;
+import java.util.List;
 import java.util.StringJoiner;
 
 public class Application implements Entity {
@@ -12,6 +13,8 @@ public class Application implements Entity {
     private final ApplicationState state;
     private final String appId;
     private final String appInfo;
+    // For Sparkmagic compatibility
+    private final List<String> log = List.of();
     private final SubmitParams submitParams;
     private final LocalDateTime createdAt;
     private final LocalDateTime contactedAt;
@@ -48,6 +51,10 @@ public class Application implements Entity {
 
     public String getAppInfo() {
         return appInfo;
+    }
+
+    public List<String> getLog() {
+        return log;
     }
 
     public SubmitParams getSubmitParams() {
