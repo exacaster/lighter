091a960ae3eeba6ed952e8102bb87687b1cab9ea
Denisas <denisas.zeromskis@exacaster.com>
2021-09-20 17:01:34 +0300

    KTT-2939 Fix Yarn response parsing issue

--- server/src/main/java/com/exacaster/lighter/backend/yarn/resources/YarnApplication.java
+++ server/src/main/java/com/exacaster/lighter/backend/yarn/resources/YarnApplication.java
@@ -1,11 +1,13 @@
 package com.exacaster.lighter.backend.yarn.resources;
 
 import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import io.micronaut.core.annotation.Introspected;
 import io.micronaut.core.annotation.Nullable;
 
 @Introspected
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class YarnApplication {
     private final String id;
     private final String trackingUrl;
