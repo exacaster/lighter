bbf594132dd3716bbfe67801d8074a2b22d24d02
Minutis <minutis@exacaster.com>
2022-01-06 11:16:51 +0200

    Reformat Batches window Add Spark submit command to Batches window

--- frontend/src/pages/Batch.tsx
+++ frontend/src/pages/Batch.tsx
@@ -27,12 +27,16 @@ const Batch: React.FC = () => {
           </Thead>
           <Tbody>
             <Tr>
-              <Td>Name (--name)</Td>
-              <Td>{batch.submitParams.name}</Td>
-            </Tr>
-            <Tr>
               <Td>File</Td>
               <Td>{batch.submitParams.file}</Td>
+            </Tr>
+            <Tr>
+              <Td>Args</Td>
+              <Td>{batch.submitParams.args.join(', ')}</Td>
+            </Tr>
+            <Tr>
+              <Td>Name (--name)</Td>
+              <Td>{batch.submitParams.name}</Td>
             </Tr>
             <Tr>
               <Td>Driver Cores (--driver-cores)</Td>
@@ -55,10 +59,6 @@ const Batch: React.FC = () => {
               <Td>{batch.submitParams.executorMemory}</Td>
             </Tr>
             <Tr>
-              <Td>Args</Td>
-              <Td>{batch.submitParams.args.join(', ')}</Td>
-            </Tr>
-            <Tr>
               <Td>Python files (--py-files)</Td>
               <Td>{batch.submitParams.pyFiles.join(', ')}</Td>
             </Tr>
@@ -74,13 +74,9 @@ const Batch: React.FC = () => {
               <Td>Additional JARs (--jars)</Td>
               <Td>{batch.submitParams.jars.join(', ')}</Td>
             </Tr>
-            <Tr>
-              <Th>Config (--conf)</Th>
-              <Th>Value</Th>
-            </Tr>
             {Object.entries(batch.submitParams.conf).map(([name, val]) => (
               <Tr key={name}>
-                <Td>{name}</Td>
+                <Td>--conf {name}</Td>
                 <Td>{val}</Td>
               </Tr>
             ))}
@@ -90,11 +86,70 @@ const Batch: React.FC = () => {
     );
   }, [batch]);
 
+  function getSparkSubmitArg(key: string, value: string) {
+    if (value) {
+      return ' ' + key + ' ' + value;
+    } else {
+      return '';
+    }
+  }
+
+  const sparkSubmitStr = useMemo(() => {
+    if (!batch) {
+      return null;
+    }
+
+    return (
+      <Code className={styles.logs}>
+        spark-submit
+        {getSparkSubmitArg('--name', batch.submitParams.name)}
+        {getSparkSubmitArg('--driver-cores', batch.submitParams.driverCores.toString())}
+        {getSparkSubmitArg('--driver-memory', batch.submitParams.driverMemory)}
+        {getSparkSubmitArg('--num-executors', batch.submitParams.numExecutors.toString())}
+        {getSparkSubmitArg('--executor-cores', batch.submitParams.executorCores.toString())}
+        {getSparkSubmitArg('--executor-memory', batch.submitParams.executorMemory)}
+        {getSparkSubmitArg('--py-files', batch.submitParams.pyFiles.join(','))}
+        {getSparkSubmitArg('--archives', batch.submitParams.archives.join(','))}
+        {getSparkSubmitArg('--files', batch.submitParams.files.join(','))}
+        {getSparkSubmitArg('--jars', batch.submitParams.jars.join(','))}
+        {Object.entries(batch.submitParams.conf).map(([name, val]) => (
+          <span key={name}>
+            --conf {name} {val + ' '}
+          </span>
+        ))}
+        {' ' + batch.submitParams.file}
+        {' ' + batch.submitParams.args.join(' ')}
+      </Code>
+    );
+  }, [batch]);
+
+  const logsStr = useMemo(() => {
+    if (!logs) {
+      return null;
+    }
+
+    if (logs.log.startsWith('http')) {
+      return <a href={logs.log}>{logs?.log}</a>;
+    } else {
+      return <Code className={styles.logs}>{logs.log}</Code>;
+    }
+  }, [logs]);
+
   return (
     <div className={styles.batch}>
       <PageHeading>Batch {id}</PageHeading>
-      <Code className={styles.logs}>{logs?.log}</Code>
+      <Box mt="5">
+        <b>Logs:</b>
+        <br />
+        {logsStr}
+      </Box>
+
       {appInfo}
+      <Box mt="5">
+        <b>Spark submit command:</b>
+        <br />
+        {sparkSubmitStr}
+      </Box>
     </div>
   );
 };
