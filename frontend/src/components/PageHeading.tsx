46c1433e6e0169bd942095cafb789511c803230c
paulius <paulius@exacaster.com>
2022-11-22 12:28:45 +0200

    Update application title

--- frontend/src/components/PageHeading.tsx
+++ frontend/src/components/PageHeading.tsx
@@ -1,14 +1,15 @@
-import {Heading} from '@chakra-ui/layout';
+import {Heading, HeadingProps} from '@chakra-ui/layout';
 import {ReactNode} from 'react';
 
 interface Props {
   children: ReactNode;
+  mb?: HeadingProps['mb'];
 }
 
-const PageHeading: React.FC<Props> = ({children}) => {
+const PageHeading: React.FC<Props> = ({children, mb = '5'}) => {
   return (
     <>
-      <Heading size="lg" mb="5">
+      <Heading size="lg" mb={mb}>
         {children}
       </Heading>
     </>
